cbedfc207b920e8f584dab59bc0744705c8504dd
kaeru-009 <shuuto-sakihama-19h@stu.kbc.ac.jp>
2020-10-22 18:32:33 +0900

    ヘビの移動(合体)

---
+++ predation/Assets/C#/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public Animator anim;
+
+    private Rigidbody rB;
+
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float lsh = Input.GetAxis("L_Stick_H");
+
+        if (lsh != 0)
+        {
+            //再開
+            anim.SetFloat("Blend", 1.0f);
+            anim.SetBool("hebi Trigger", true);
+            float x = Input.GetAxis("Horizontal");
+            //float z = Input.GetAxis("Vertical");
+            //rB.AddForce(x * speed, 0, z * speed, ForceMode.Impulse);
+            rB.AddForce(x * speed, 0, 0, ForceMode.Impulse);
+            Debug.Log("L_Stick_H");
+
+        }
+        else if (lsh == 0)
+        {
+            anim.SetBool("hebi Trigger", false);
+            //一時停止
+            anim.SetFloat("Blend", 0.0f);
+            Debug.Log("animT");
+        }
+    }
+}
