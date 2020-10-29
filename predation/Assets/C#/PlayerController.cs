44bb7836395e0ae64c62a13d7486d25efdbe23a6
kaeru-009 <shuuto-sakihama-19h@stu.kbc.ac.jp>
2020-10-29 16:10:58 +0900

    最新ヘビ

--- predation/Assets/C#/PlayerController.cs
+++ predation/Assets/C#/PlayerController.cs
@@ -8,35 +8,49 @@ public class PlayerController : MonoBehaviour
     public float speed = 0.0f;
     public Animator anim;
 
-    private Rigidbody rB;
+    private CharacterController _characterController;
+    private Vector3 _moveVelocity;
+    //private Rigidbody rB;
 
     void Start()
     {
-        rB = GetComponent<Rigidbody>();
+        //rB = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         float lsh = Input.GetAxis("L_Stick_H");
 
-        if (lsh != 0)
+        AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+        float duration = currentState.length;
+
+        //Yボタンが押されてかの判定
+        if (Input.GetKey("joystick button 3"))
+        {
+            anim.SetBool("kami Trigger", true);
+            Debug.Log("button3");
+           
+        }
+        
+        //Lステックが左右に傾いたときの判定
+        if (lsh != 0) 
         {
             //再開
             anim.SetFloat("Blend", 1.0f);
             anim.SetBool("hebi Trigger", true);
-            float x = Input.GetAxis("Horizontal");
-            //float z = Input.GetAxis("Vertical");
+            _moveVelocity.x = Input.GetAxis("Horizontal") * speed;
             //rB.AddForce(x * speed, 0, z * speed, ForceMode.Impulse);
-            rB.AddForce(x * speed, 0, 0, ForceMode.Impulse);
+            //rB.AddForce(x * speed, 0, 0, ForceMode.Impulse);
+            _characterController.Move(_moveVelocity * Time.deltaTime);
             Debug.Log("L_Stick_H");
-
         }
         else if (lsh == 0)
         {
             anim.SetBool("hebi Trigger", false);
             //一時停止
             anim.SetFloat("Blend", 0.0f);
-            Debug.Log("animT");
+            //Debug.Log("animT");
         }
     }
 }
